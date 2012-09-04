954ec2398c089e10405728e097473d52b20f1ad0
baden.i.ua@gmail.com <baden.i.ua@gmail.com@cd201f0b-5521-6f96-0748-8efd02dae0ad>
2012-09-04 09:22:49 +0000

    Поддержка типов параметров LONG и STR?.

--- trunk/test-python/api/testconfig.py
+++ trunk/test-python/api/testconfig.py
@@ -12,6 +12,13 @@ def main():
 	for x in xrange(10):
 		body += 'P%s INT 10 10\n' % str(x)
 
+	body += 'testlong LONG 10 10\n'
+	body += 'teststr16 STR16 "192.168.0.1" "192.168.0.1"\n'
+	body += 'teststr32 STR32 "baden.gps.navi.cc:8000/service" "baden.gps.navi.cc:8000/service"\n'
+	body += 'teststr32ws STR32 "data body" "data body"\n'
+	body += 'teststr32wq STR32 "data body \"body\" quote" "data body \"body\" quote"\n'
+	body += 'teststr32wms STR32 "add some      spaces" ""\n'
+
 	rawPOST2('/config?imei=%s&cmd=save&csq=17&vout=580&vin=3500&phone=1234566778' % (IMEI), body)
 
 if __name__ == "__main__":
